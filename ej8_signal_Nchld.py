8f9c54fedd1da3b02d49962c15e6f479d8384419
YoelRoger <yoelfernandez2008@gmail.com>
2020-05-01 20:03:06 -0300

    ej8 ready 4 tests :D

---
+++ ej8_signal_Nchld.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+import signal
+import time
+import os
+import sys
+import getopt
+
+
+def handlerUSR2(signal, frame):
+    print("Soy el PID>> ", os.getpid(), "recibí la señal>> ", signal, "de mi padre PID>>", os.getppid())
+
+
+def main():
+    (opts, args) = getopt.getopt(sys.argv[1:], 'p:', ['process='])
+
+    if len(opts) != 2:
+        print("no se ingreso la cantidad de parametros y argumentos correctos")
+        exit()
+
+    for opt, value in opts:
+        if opt == "-p" or opt == "--process":
+            num_chld = int(value)
+
+            # CANT HIJOS
+            for x in range(num_chld):
+                pid1 = os.fork()
+                # PADRE
+                if pid1:
+                    time.sleep(1)
+                    print("Creando proceso: ", pid1, "\n\n")
+                    send_signal(pid1)
+                    os.wait()
+                    print("TERMINA2")
+                # HIJO
+                else:
+                    signal.signal(signal.SIGUSR2, handlerUSR2)
+                    signal.pause()
+                    exit(0)
+
+
+def send_signal(pid):
+    pid = int(pid)
+    os.kill(pid, signal.SIGUSR2)
+
+
+if __name__ == '__main__':
+    main()
