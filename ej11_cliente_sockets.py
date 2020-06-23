f64e94b5374f02bebce40949153c4fcc45305208
YoelRoger <yoelfernandez2008@gmail.com>
2020-06-23 13:57:59 -0300

    ej 11 fixed, added response control

--- ej11_cliente_sockets.py
+++ ej11_cliente_sockets.py
@@ -1,77 +1,104 @@
 #!/usr/bin/python3
 import sys
 import socket
+import getopt
 # CLIENTE PARA SERVIDOR TCP
 
-try:
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-except socket.error:
-    print('FAILED TO CREATE SOCKET')
-    sys.exit()
 
-# set host
-host = '167.71.159.138'
-port = 8000
+def main():
+    host = None
+    port = None
+    code = None
+    # adding response control
 
-s.connect((host, port))
-print("CONNECTED")
+    def reply():
+        if respuesta == 200:
+            print("Server reply: 200 - OK")
+        elif respuesta == 400:
+            print("Server reply: 400 - Comando válido, pero fuera de secuencia.")
+        elif respuesta == 500:
+            print("Server reply: 500 - Comando invalido")
+        elif respuesta == 404:
+            print("Server reply: 404 - Clave erronea")
+        elif respuesta == 405:
+            print("Server reply: 405 - Cadena nula")
 
-while True:
-
-    msgin = input('ENTER NAME : ').encode()
     try:
-        # enviar mensaje str
-        msg = 'hello|' + msgin
-        s.send(msg.encode('ascii'))
+        (opt, arg) = getopt.getopt(sys.argv[1:], 'h:p:', ["host=", "port="])
+    except getopt.GetoptError as err:
+        print("ERROR", str(err))
+        exit()
+    for (option, arg) in opt:
+        if option == '-h' or option == '--host':
+            host = arg
+        elif option == '-p' or option == '--port':
+            port = int(arg)
 
-        # recibir data  (data, addr)
-        respuesta = s.recv(1024)
-
-        print('RESPUESTA DEL SV : ' + respuesta.decode("ascii"))
-
-    except socket.error:
-        print('CODIGO DE ERROR> : ' + str(msg[0]) + ' Message ' + msg[1])
-        sys.exit()
-
-    msgin = input('ENTER EMAIL : ').encode()
     try:
-        # enviar mensaje str
-        msg = 'email|' + msgin
-        s.send(msg.encode('ascii'))
-
-        # recibir data  (data, addr)
-        respuesta = s.recv(1024)
-
-        print('RESPUESTA DEL SV : ' + respuesta.decode("ascii"))
-
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     except socket.error:
-        print('CODIGO DE ERROR> : ' + str(msg[0]) + ' Message ' + msg[1])
+        print('FAILED TO CREATE SOCKET')
         sys.exit()
 
-    msgin = input('ENTER KEY : ').encode()
-    try:
-        # enviar mensaje str
-        msg = 'key|' + msgin
-        s.send(msg.encode('ascii'))
+    s.connect((host, port))
+    print("CONNECTED")
 
-        # recibir data  (data, addr)
-        respuesta = s.recv(1024)
+    while True:
 
-        print('RESPUESTA DEL SV : ' + respuesta.decode("ascii"))
+        msgin = input('ENTER NAME : ')
+        try:
+            # enviar mensaje str
+            msg = 'hello|' + msgin
+            s.send(msg.encode('ascii'))
 
-    except socket.error:
-        print('CODIGO DE ERROR> : ' + str(msg[0]) + ' Message ' + msg[1])
-        sys.exit()
+            # recibir data
+            resp = s.recv(1024)
+            respuesta = int(resp.decode("ascii"))
+            reply()
 
-    msg = 'exit'
-    try:
-        print("SENDING EXIT")
-        s.send(msg)
-        respuesta = s.recv(1024)
+        except socket.error:
+            print('CODIGO DE ERROR> : ' + str(msg[0]) + ' Message ' + msg[1])
+            sys.exit()
 
-        print('RESPUESTA DEL SV: ' + respuesta.decode("ascii"))
-        sys.exit()
-    except socket.error:
-        print('CODIGO DE ERROR> ' + str(msg[0]) + ' Message ' + msg[1])
-        sys.exit()
-s.close()
+        msgin = input('ENTER EMAIL : ')
+        try:
+            # enviar mensaje str
+            msg = 'email|' + msgin
+            s.send(msg.encode('ascii'))
+
+            # recibir data
+            resp = s.recv(1024)
+            respuesta = int(resp.decode("ascii"))
+            reply()
+
+        except socket.error:
+            print('CODIGO DE ERROR> : ' + str(msg[0]) + ' Message ' + msg[1])
+            sys.exit()
+
+        while not code == 200:
+            msgin = input('ENTER KEY : ')
+            msg = 'key|' + msgin
+            s.send(msg.encode('ascii'))
+
+            # recibir data
+            resp = s.recv(1024)
+            respuesta = int(resp.decode("ascii"))
+            code = respuesta
+            reply()
+
+        msg = 'exit'.encode()
+        try:
+            print("SENDING EXIT")
+            s.sendto(msg, (host, port))
+            respuesta = s.recv(1024)
+
+            print('RESPUESTA DEL SV: ' + respuesta.decode("ascii"))
+            sys.exit()
+
+        except socket.error:
+            print('CODIGO DE ERROR> ' + str(msg[0]) + ' Message ' + msg[1])
+            sys.exit()
+
+    s.close()
+    if __name__ == '__main__':
+        main()
