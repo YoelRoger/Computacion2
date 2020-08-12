7bbe5cba3a89f32cbdc989f2616baf7775011c29
YoelRoger <yoelfernandez2008@hotmail.com>
2020-08-12 12:39:32 -0300

    ej 17 structured

---
+++ ej17_echo_inv/ej17_client.py
@@ -0,0 +1,11 @@
+import socket, getopt
+from sys import argv, exit, stdin
+
+
+def getOp():
+    try:
+        (opts, args) = getopt.getopt(argv[1:], 'p:h:', [])
+        return opts
+    except getopt.GetoptError as error:
+        print("COULD'T SUCCEED ERROR:", str(error))
+        exit()
---
+++ ej17_echo_inv/ej17_server.py
@@ -0,0 +1,44 @@
+import socket, getopt
+from sys import argv
+
+
+def main():
+
+    def getOp():
+        try:
+            (opts, args) = getopt.getopt(argv[1:], 'p:', [])
+            return opts
+        except getopt.GetoptError as error:
+            print("COULD'T SUCCEED :C ERROR>>>", str(error))
+            exit()
+
+    options = getOp()
+    for (opts, arg) in options:
+        if opts == '-p':
+            port = int(arg)
+
+    def invertMsg(cadena):
+        return cadena[::-1]
+
+    def socketStructure(port):
+        port = port
+
+        serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        host = ""
+        serversocket.bind((host, port))
+        serversocket.listen(5)
+        print('SERVER LISTENING ...')
+        clientsocket, addr = serversocket.accept()
+
+        while True:
+            d = clientsocket.recv(1024)
+            recived = d.decode("ascii") + '\n'
+            msgReversed = invertMsg(recived)
+            socket.send(msgReversed.encode('assci'))
+            print("Address: %s " % str(addr))
+            print("Received correctly: " + d.decode("ascii"))
+
+    socketStructure(port)
+
+ main()
+
