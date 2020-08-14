441057c09c5b5f4c1b8ff582aba7e8c75220e6ac
YoelRoger <yoelfernandez2008@hotmail.com>
2020-08-14 14:56:07 -0300

    ej 17 something broken :c

--- ej17_echo_inv/ej17_client.py
+++ ej17_echo_inv/ej17_client.py
@@ -10,3 +10,34 @@ def getOp():
     except getopt.GetoptError as error:
         print("COULD'T SUCCEED ERROR:", str(error))
         exit()
+
+
+def socketStructure(host, port):
+    host = host
+    port = port
+
+    try:
+        socketCreated = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    except socket.error:
+        print("FAILED TO CREATE SOCKET")
+    exit()
+    socketCreated.connect((host, port))
+    while True:
+        try:
+            msgin = input('ENTER MESSAGE ....>>>>>> ')
+            socketCreated.send(msgin.encode('assci'))
+        except EOFError:
+            break
+
+
+def main():
+    options = getOp()
+    for (opts, args) in options:
+        if opts == '-p':
+            port = int(args)
+        if opts == '-h':
+            host = args
+    socketStructure(host, port)
+
+
+main()
