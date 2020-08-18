1d586cbdce8dd71b5e3d420439112df6b04ec6be
YoelRoger <yoelfernandez2008@hotmail.com>
2020-08-18 19:55:08 -0300

    nothing

--- ej17_echo_inv/ej17_server.py
+++ ej17_echo_inv/ej17_server.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python
-import socket, getopt
+import getopt
+import socket
 from sys import argv
 
 
 def main():
 
-    def getOp():
+    port = None
+
+    def get_op():
         try:
             (opts, args) = getopt.getopt(argv[1:], 'p:', [])
             return opts
@@ -13,20 +16,20 @@ def main():
             print("COULD'T SUCCEED :C ERROR>>>", str(error))
             exit()
 
-    options = getOp()
+    options = get_op()
     for (opts, arg) in options:
         if opts == '-p':
             port = int(arg)
 
-    def invertMsg(cadena):
+    def invert_msg(cadena):
         return cadena[::-1]
 
-    def socketStructure(port):
-        port = port
+    def socket_structure(selected_port):
+        selected_port = selected_port
 
         serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         host = ""
-        serversocket.bind((host, port))
+        serversocket.bind((host, selected_port))
         serversocket.listen(5)
         print('SERVER LISTENING ...')
         clientsocket, addr = serversocket.accept()
@@ -34,14 +37,13 @@ def main():
         while True:
             d = clientsocket.recv(1024)
             recived = (d.decode("ascii"))
-            msgReversed = invertMsg(recived)
-            serversocket.send(msgReversed.encode('assci'))
+            msg_reversed = invert_msg(recived)
+            serversocket.send(msg_reversed.encode('assci'))
             print("Address: %s " % str(addr))
             print("Received correctly: " + d.decode("ascii"))
 
-    socketStructure(port)
+    socket_structure(port)
 
 
 if __name__ == '__main__':
     main()
-
